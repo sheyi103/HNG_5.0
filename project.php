a0a49348e661cc2218c7ef6b09376f2abcacfe4d
sheyi103 <sheyi103@outlook.com>
2019-03-30 19:30:47 +0100

    Adding all files together

--- project.php
+++ project.php
@@ -5,10 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HNG 5.0</title>
+    <title>Project Page</title>
 </head>
 <body>
-<p>The only competitor i have is myself</p>
-<h1>i'm the best</h1>
+<p>Wlcome to the project page!</p>
 </body>
 </html>
